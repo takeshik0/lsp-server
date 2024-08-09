77761a359387e7f5ea47909f4dd705eb7c29196a
takeshi <volosukdima86@gmail.com>
2024-08-09 13:27:11 +0300

    Server added

--- include/JsonRpc.hpp
+++ include/JsonRpc.hpp
@@ -1,24 +1,25 @@
 #pragma once
-#include <string>
 #include <fstream>
+#include <string>
 
 class JsonRpc {
 private:
-    std::string m_jsonrpc;
-    std::string m_jsonrpc_version;
-    std::string m_method;
-    std::string m_params;
-    std::string m_id;
+  std::string m_jsonrpc;
+  std::string m_jsonrpc_version;
+  std::string m_method;
+  std::string m_params;
+  std::string m_id;
+    
+  std::string jsonToString(std::string jsonFilePath);
+  void parseArgument(const std::string &typeOfArgument, std::string &argument);
 
-    std::string jsonToString(std::string jsonFilePath);
-    void parseArgument(const std::string &typeOfArgument, std::string &argument);
+public:
+  std::string getJsonrpc();
+  std::string getJsonrpcVersion();
+  std::string getMethod();
+  std::string getParams();
+  std::string getId();
 
-public: 
-    std::string getJsonrpcVersion();
-    std::string getMethod();
-    std::string getParams();
-    std::string getId();
-
-    void parseRequest(std::string& jsonFilePath);
-    JsonRpc() = default;
+  void parseRequest(std::string &jsonFilePath);
+  JsonRpc() = default;
 };
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,20 @@
 #include "Server.hpp"
 #include "GotoDefinition.hpp"
 #include <fstream>
+#include <string>
 
 void Server::gotoDefinition(JsonRpc &json) {
   GotoDefinition method;
   method.parseParams(json.getParams());
   
   std::ifstream file(method.getUri());
+  std::string line;
+  for(int i = 0; i < method.getLine(); i++) {
+    std::getline(file, line);
+  }
+  
+  std::string wordToSearch = line.substr(method.getStartCharacter(), method.getEndCharacter() - method.getStartCharacter());
 
+  int definition = 
 
 }
