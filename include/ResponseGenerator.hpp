600a980ee0fdfcdced7e13f5202af4d88e2a199f
takeshi <dimav2206@gmail.com>
2024-09-09 18:33:34 +0300

    fix empty position

--- include/ResponseGenerator.hpp
+++ include/ResponseGenerator.hpp
@@ -4,6 +4,7 @@
 
 class ResponceGenerator {
 private:
+  std::string removeEmptyPosition(const std::string& jsonContent);
 public:
   ResponceGenerator(JsonRpc& jsonrpc, range result);
   ResponceGenerator(JsonRpc& jsonrpc, std::vector<range> result);
